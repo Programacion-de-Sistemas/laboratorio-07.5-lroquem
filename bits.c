b66825b1320ca6230b977aab19d9e18798549215
Luis Eduardo Roque Mamani <lroquem@unsa.edu.pe>
2022-06-29 22:27:23 -0500

    isAsciiDigit: funcion terminada

--- bits.c
+++ bits.c
@@ -187,7 +187,7 @@ int allOddBits(int x) {
  *   Rating: 2
  */
 int negate(int x) {
-  return 2;
+  return ~x + 1;
 }
 //3
 /* 
@@ -200,7 +200,9 @@ int negate(int x) {
  *   Rating: 3
  */
 int isAsciiDigit(int x) {
-  return 2;
+  int gte = (x + (~0x30 + 1))>>31;
+  int lte = (0x39 + (~x + 1))>>31;
+  return !gte & !lte;
 }
 /* 
  * conditional - same as x ? y : z 
